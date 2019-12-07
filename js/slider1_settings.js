da3f74aa99089927f849aa1c22b24b4ef095099f
artofnext <novnovalex@gmail.com>
2019-12-07 12:31:02 +0200

    add a lot

---
+++ js/slider1_settings.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slide1').slick({
+      // setting-name: setting-value
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+      dots: true,
+      // centerMode: true,
+      // slidesToShow: 1,
+      variableWidth: true,
+    //   vertical: true,
+    });
+  });
