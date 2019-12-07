b03a98811f27469c016e598a56050bc518208b8f
artofnext <novnovalex@gmail.com>
2019-12-07 13:29:01 +0200

    add carousel 2

---
+++ js/slider2_settings.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slide2').slick({
+      // setting-name: setting-value
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: true,
+      dots: true,
+      // centerMode: true,
+      slidesToShow: 3,
+      variableWidth: true,
+    //   vertical: true,
+    });
+  });
