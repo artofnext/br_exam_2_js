dafcde044989a5f774a891ee5e6d1b53ebcae999
artofnext <novnovalex@gmail.com>
2019-12-10 17:42:28 +0200

    map setted ut, contact & footer added

--- js/map.js
+++ js/map.js
@@ -1,7 +1,6 @@
-// Geolocation.getCurrentPosition();
 
 // initialize Leaflet
-var map = L.map('map').setView({lon: 0, lat: 0}, 2);
+var map = L.map('map').setView({lon: 28.437470, lat: 49.218677}, 16); 
 
 // add the OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -13,4 +12,29 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.control.scale().addTo(map);
 
 // show a marker on the map
-L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+// L.marker({lon: 31.1, lat: 48.3}).bindPopup('The center of the world').addTo(map);
+
+// get user geolocation
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+
+function success(pos) {
+    var crd = pos.coords;
+    
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+    
+    L.marker({lon: crd.longitude, lat: crd.latitude}).bindPopup('You are here').addTo(map);
+    // map = L.map('map').setView({lon: crd.latitude, lat: crd.longitude}, 8);
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
