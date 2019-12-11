f87fb56b46a0e42984bf417f92b809306ad378ac
artofnext <novnovalex@gmail.com>
2019-12-11 16:11:07 +0200

    scroll contact bugfix

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $( "a.scroll_link" ).click(function( event ) {
         event.preventDefault();
-        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 1500);
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 1000);
     });
 });
