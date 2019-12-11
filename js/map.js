b8f738880f3260a72126d1442c68d946d9c28ecf
artofnext <novnovalex@gmail.com>
2019-12-11 16:10:43 +0200

    map marker bugfix

--- js/map.js
+++ js/map.js
@@ -1,6 +1,5 @@
-
 // initialize Leaflet
-var map = L.map('map').setView({lon: 28.437470, lat: 49.218677}, 16); 
+let map = L.map('map').setView({lon: 28.437470, lat: 49.218677}, 16); 
 
 // add the OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,29 +11,30 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.control.scale().addTo(map);
 
 // Custom marker
-var customIcon = L.icon({
-  iconUrl: '../img/insta_ico.png',
-  shadowUrl: '../img/insta_ico.png',
+let customIcon = L.icon({
+  iconUrl: '../img/Logo_marker.svg',
+  shadowUrl: '../img/Logo_shadow.svg',
 
-  iconSize:     [40, 60], // size of the icon
-  shadowSize:   [40, 40], // size of the shadow
-  iconAnchor:   [20, 59], // point of the icon which will correspond to marker's location
-  shadowAnchor: [20, 39],  // the same for the shadow
-  popupAnchor:  [-3, -60] // point from which the popup should open relative to the iconAnchor
+  iconSize:     [100, 100], // size of the icon
+  shadowSize:   [100, 100], // size of the shadow
+  iconAnchor:   [10, 100], // point of the icon which will correspond to marker's location
+  shadowAnchor: [0, 75],  // the same for the shadow
+  popupAnchor:  [-3, -70] // point from which the popup should open relative to the iconAnchor
 });
 
 // show a marker on the map
-L.marker([28.437470, 49.218677], {icon: customIcon}).bindPopup('Custom marker').addTo(map);
+L.marker({lon: 28.437470, lat: 49.218677}).bindPopup('Here we are!').addTo(map);
+L.marker({lon: 28.437470, lat: 49.218677}, {icon: customIcon}).bindPopup('Here we are!').addTo(map);
 
 // get user geolocation
-var options = {
+let options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
 };
 
 function success(pos) {
-    var crd = pos.coords;
+    let crd = pos.coords;
     
     console.log('Your current position is:');
     console.log(`Latitude : ${crd.latitude}`);
@@ -42,7 +42,6 @@ function success(pos) {
     console.log(`More or less ${crd.accuracy} meters.`);
     
     L.marker({lon: crd.longitude, lat: crd.latitude}).bindPopup('You are here').addTo(map);
-    // map = L.map('map').setView({lon: crd.latitude, lat: crd.longitude}, 8);
   }
   
   function error(err) {
